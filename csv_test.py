2d63091e67451dcc94683b8cab86a73ef9ef9544
DzhEgo <maximalkin2005@gmail.com>
2023-12-05 13:23:20 +0300

    2

--- csv_test.py
+++ csv_test.py
@@ -11,13 +11,9 @@ with open('files/grades.csv', mode='r', newline='') as csv_file:
         tests = [float(row[f'Test{i}']) for i in range(1, 5)]
         valid_tests = [test for test in tests if test >= 0]
 
-        if valid_tests:
-            average_grade = sum(valid_tests) / len(valid_tests)
-        else:
-            average_grade = 0.0
+        average_grade = sum(valid_tests) / len(valid_tests) if valid_tests else 0.0
 
         data[(last_name, first_name, ssn)] = average_grade
-
 
 with open('files/new.csv', mode='w', newline='') as csv_file:
     fieldnames = ['Last name', 'First name', 'SSN', 'Average Grade']
@@ -26,7 +22,5 @@ with open('files/new.csv', mode='w', newline='') as csv_file:
     csv_writer.writeheader()
 
     for (last_name, first_name, ssn), average_grade in data.items():
-        csv_writer.writerow(
-        {'Last name': last_name, 'First name': first_name, 'SSN': ssn, 'Average Grade': average_grade})
+        csv_writer.writerow({'Last name': last_name, 'First name': first_name, 'SSN': ssn, 'Average Grade': average_grade})
 print("Новый файл успешно создан.")
-
